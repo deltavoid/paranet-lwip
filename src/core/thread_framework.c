a5e509c386314a7cb3f94c3550b7758e2c193756
zqy <850074816@qq.com>
2024-12-10 22:22:35 +0800

    add tcp_thread_run and tcp_thread_init

--- src/core/thread_framework.c
+++ src/core/thread_framework.c
@@ -1,16 +1,27 @@
+/*
+ * thread framework
+ * @author ZQY
+*/
 
+#include <rte_ring.h>
+#include <rte_malloc.h>
+#include <rte_eal.h>
 
 #include "lwip/thread_framework.h"
 
 #include "lwip/logging.h"
-
+#include <pthread.h>
+#include <unistd.h>
 
 
 
 // tcp_thread class ----------------------------
 
-struct tcp_thread_cb {
+struct tcp_thread_ctx {
     int id;
+    volatile int running;
+    pthread_t pthread_ctx;
+
 
 
     // todo, dpdk ring for tcp pkt in process
@@ -19,19 +30,39 @@ struct tcp_thread_cb {
 
 };
 
-// void tcp_thread_init(struct tcp_thread_cb* cb)
-// {
+void* tcp_thread_run(void* arg)
+{
+    struct tcp_thread_ctx* ctx = (struct tcp_thread_ctx*)arg;
 
-// }
+    // todo, eventloop, while(epoll) { process event}
+    // at first stage, just use eventfd as entry.
 
-// void tcp_thread_run(struct tcp_thread_cb* cb)
-// {
+    while (ctx->running)
+    {
+        LOG_DEBUG("tcp_thread_run, id: %d\n", ctx->id);
 
-//     // todo, eventloop, while(epoll) { process event}
-//     // at first stage, just use eventfd as entry.
+        sleep(1);        
+    }
+
+    return NULL;
+}
+
+int tcp_thread_init(struct tcp_thread_ctx* ctx, int id)
+{
+    ctx->id = id;
+    ctx->running = true;
+
+    // create pthread
+    int ret = pthread_create(&ctx->pthread_ctx, NULL, tcp_thread_run, ctx);
+    if  (ret != 0)
+    {   LOG_INFO("pthread create failed\n");
+        return ret;
+    }
+
+    return ret;
+}
 
 
-// }
 
 void tcp_thread_destroy()
 {
@@ -56,6 +87,10 @@ void ip_thread_run()
 
 
 // thread framework object -------------------------
+
+#define TCP_THREAD_MAX_NUM 32
+#define IP_THREAD_MAX_NUM 8 
+
 
 int g_tcp_thread_num, g_ip_thread_num; // global variable, init at process initialization, and should not be changed after that.
 
