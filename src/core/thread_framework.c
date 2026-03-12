3cd86383e2a89863e39ed8f24a374fa9cc363bd4
zqy <850074816@qq.com>
2026-03-12 17:19:56 +0800

    add tcp_thread_run definition

--- src/core/thread_framework.c
+++ src/core/thread_framework.c
@@ -5,7 +5,9 @@
 #include "lwip/logging.h"
 
 
-int g_tcp_thread_num, g_ip_thread_num; // global variable, init at process initialization, and should not be changed after that.
+
+
+// tcp_thread class ----------------------------
 
 struct tcp_thread_cb {
     int id;
@@ -13,14 +15,51 @@ struct tcp_thread_cb {
 
     // todo, dpdk ring for tcp pkt in process
 
+    // todo, tcp thread pkt output queue
+
 };
 
+void tcp_thread_init(struct tcp_thread_cb* cb)
+{
+
+}
+
+void tcp_thread_run(struct tcp_thread_cb* cb)
+{
+
+    // todo, eventloop, while(epoll) { process event}
+    // at first stage, just use eventfd as entry.
+
+
+}
+
+void tcp_thread_destroy()
+{
+}
+
+
+// ip thread class ---------------------------
 struct ip_thread_cb {
     int id;
 
+    // todo, pkt input entry.
+
 };
 
+void ip_thread_run()
+{
+    // death loop  poll pkt only wait process exit
+    
+}
+
 // todo, thread eventloop, base on epoll and eventfd.
+
+
+// thread framework object -------------------------
+
+int g_tcp_thread_num, g_ip_thread_num; // global variable, init at process initialization, and should not be changed after that.
+
+// todo, tcp_thread_cb list, ip_thread_cb_list
 
 
 void thread_framework_init(int ip_thread_num, int tcp_thread_num)
